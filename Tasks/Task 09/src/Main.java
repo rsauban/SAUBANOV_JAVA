92e33c018c308bf79b5203d056429719c5fa3298
Саубанов Рустам Михайлович <sauban@spo-4.npovs.icl.kazan.ru>
2019-01-24 16:15:50 +0300

    add Task 10

--- Tasks/Task 09/src/Main.java
+++ Tasks/Task 09/src/Main.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
 
+import static java.lang.Math.pow;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        char arrayInt = scanner.nextLine().toCharArray();
+        char arrayChar [] = scanner.nextLine().toCharArray();
+        int numberInt = 0;
+        for (int i = 0; i < arrayChar.length; i++){
+            // число = цифра1 * (10 в степени позиция цифры) +  цифра2 *  ...
+            numberInt = (int) ((arrayChar[i] - '0')*pow(10, arrayChar.length-i-1) + numberInt);
+                 }
+       System.out.println("int x == " + numberInt);
 
 
     }
+
 
 }
---
+++ Tasks/Task 10/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char strSource[] = scanner.nextLine().toCharArray();
+        char strSearch[] = scanner.nextLine().toCharArray();
+        int searchIndex = -1;
+        int j = 0;
+
+        for (int i = 0; i < strSource.length; i++) {
+            // берём первый элемент strSearch  ищем
+            //вхождние его в текущей строке до первого вхождения подстроки
+            if (strSearch[j] == strSource[i] && searchIndex == -1) {
+                searchIndex = i;
+                for (int k = 0; j + 1 + k < strSearch.length && searchIndex + 1 + k < strSource.length; k++)
+                    if (strSearch[j + 1 + k] == strSource[searchIndex + 1 + k]) {
+                        searchIndex = i;
+
+                    } else searchIndex = -1;
+
+
+            }
+
+        }
+
+        System.out.println(searchIndex);
+
+
+    }
+
+}
