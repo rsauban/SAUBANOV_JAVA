62084183ec23670eee43e01985996cc72e4c3766
Саубанов Рустам Михайлович <sauban@spo-4.npovs.icl.kazan.ru>
2019-01-30 17:02:04 +0300

    add Task 11 replace

--- Tasks/Task 11/src/Main.java
+++ Tasks/Task 11/src/Main.java
@@ -10,12 +10,24 @@ public class Main {
         // отсортированный список
         char arraySortingChar[] = scanner.nextLine().toCharArray();
         int arraySortingInt[];
-        arraySortingInt = new int[10];
+
 
         int numberInt = 0, k = 0;
         String tempString ="";
         boolean flag = false;
         // Удаляем пробелы и др разделители Переводим Char в Int, в водим его в массив
+
+        int arrayLength = 0;
+
+        // Определяем длину массива для инициализации
+        for (int i = 0; i < arraySortingChar.length;  i++) {
+            // формируем строку из цифр разедитель пробел
+            if (arraySortingChar[i] == ' ') {
+                arrayLength++;
+            }
+            }
+         // инициализируем массив
+         arraySortingInt = new int[arrayLength+1];
 
         // считываем всю строку по символьно
         for (int i = 0; i < arraySortingChar.length;  i++){
@@ -55,6 +67,39 @@ public class Main {
             }
 
         for (int i = 0; i < arraySortingInt.length; i++) System.out.println(arraySortingInt[i]);
+
+
+        //  Дихотомический поиск
+
+        int findNum = scanner.nextInt();
+       // System.out.println(findNum);
+
+        int left = 0, middle = 0;
+        int rigth = arraySortingInt.length;
+
+        while (left < rigth){
+
+            middle = left +(rigth-left)/2;
+
+            if (arraySortingInt[middle] == findNum) {System.out.println("Index = "+middle); return;}
+
+
+            if (arraySortingInt[middle] >= findNum)
+            {
+                rigth = middle;
+
+
+
+            }
+            else {
+                left = middle+1;
+
+
+            }
+            System.out.println("left = " + left + " , rigth = " + rigth + " , middle = " + middle);
+        }
+
+        System.out.println("Не найдено !!!");
 
 
 
